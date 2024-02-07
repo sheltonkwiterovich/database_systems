ca06462befe3f32b6808eabb33f967535d33d68d
jameshawkins26 <jrhawkins610@gmail.com>
2024-02-07 01:06:20 -0500

    Initialized Spring Boot backend. Currently renders 'Hello' at localhost:8080.

---
+++ backend/great-listens/src/main/java/com/database/greatlistens/GreatListensApplication.java
@@ -0,0 +1,13 @@
+package com.database.greatlistens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatListensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatListensApplication.class, args);
+	}
+
+}
---
+++ backend/great-listens/src/test/java/com/database/greatlistens/GreatListensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.database.greatlistens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatListensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
